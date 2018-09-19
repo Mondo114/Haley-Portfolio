e54bd2eed9df444509c4b128deaf7ce600282ac5
Brenton <brentonjjenkins@gmail.com>
2018-09-19 09:24:30 -0700

    new structure

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,36 +1,36 @@
 $(document).ready(function () {
 
     // Navbar Disappear and Reappear
-    // var prevScrollpos = window.pageYOffset;
-    // window.onscroll = function () {
-    //     var currentScrollPos = window.pageYOffset;
-    //     if (prevScrollpos > currentScrollPos) {
-    //         document.getElementById("navbar").style.top = "0";
-    //     } else {
-    //         document.getElementById("navbar").style.top = "-70px";
-    //     }
-    //     prevScrollpos = currentScrollPos;
-    // }
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("navbar").style.top = "0";
+        } else {
+            document.getElementById("navbar").style.top = "-70px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
 
     // When the user scrolls the page, execute myFunction 
-    window.onscroll = function () {
-        myFunction()
-    };
+    // window.onscroll = function () {
+    //     myFunction()
+    // };
 
-    // Get the navbar
-    var navbar = document.getElementById("navbar");
+    // // Get the navbar
+    // var navbar = document.getElementById("navbar");
 
-    // Get the offset position of the navbar
-    var sticky = navbar.offsetTop;
+    // // Get the offset position of the navbar
+    // var sticky = navbar.offsetTop;
 
-    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-    function myFunction() {
-        if (window.pageYOffset >= sticky) {
-            navbar.classList.add("sticky")
-        } else {
-            navbar.classList.remove("sticky");
-        }
-    };
+    // // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    // function myFunction() {
+    //     if (window.pageYOffset >= sticky) {
+    //         navbar.classList.add("sticky")
+    //     } else {
+    //         navbar.classList.remove("sticky");
+    //     }
+    // };
 
     // Navbar Scrolling
     $(".banner-title").on('click', function () {
